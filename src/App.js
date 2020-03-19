72562d461b03fb13fedb8f21a787865c3eb42f18
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-03-18 23:23:38 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
-    </div>
-  );
+	return (
+		<div className="App">
+			<p>
+				Read through the instructions in the README.md file to build your NASA
+				app! Have fun 🚀! yes I will
+			</p>
+		</div>
+	);
 }
 
 export default App;
