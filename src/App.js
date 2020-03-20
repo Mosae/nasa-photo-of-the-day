4ca36cd37a9948a2f5fee2bed17126dca652d322
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-03-20 13:42:07 -0500

    MVP done

--- src/App.js
+++ src/App.js
@@ -1,39 +1,14 @@
 import React from 'react';
 import './App.css';
-//import logo
-import { Button } from 'reactstrap';
-import styled from 'styled-components';
 import PhotoDisplay from './components/PhotoDisplay.js';
-//import Footer from './components/Footer.js
-
-// const Button = styled.button`
-//     padding: 6px 10px;
-//     margin: 5px;
-//     border: none;
-//     border-radius: 3px;
-//     color: white;
-
-//     ${props => (props.type === 'primary' ? `background: #2196f3;` : null)}
-//     ${props => (props.type === 'success' ? `background: #4caf50;` : null)}
-//     ${props => (props.type === 'danger' ? `background: #f44336;` : null)}
-//     ${props => (props.type === 'warning' ? `background: #fdd835;` : null)}
-// `;
-
-const WrapperDiv = styled.div`
-	text-align: center;
-	font-family: sans-serif;
-	color: red;
-`;
 
 function App() {
 	return (
 		<div className="App">
 			<div className="header_container">
 				{/* logo comes in here later */}
-				<WrapperDiv>
-					<h1>NASA PHOTO OF THE DAY</h1>
-				</WrapperDiv>
-				<button color="danger">NASA </button>
+
+				<h1>NASA PHOTO OF THE DAY</h1>
 
 				<PhotoDisplay />
 			</div>
--- src/components/PhotoDisplay.js
+++ src/components/PhotoDisplay.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Alert, Card, CardHeader, Col, Badge, Row } from 'reactstrap';
 
 export default function PhotoDisplay() {
 	const [image, setImage] = useState([]);
@@ -16,14 +17,19 @@ export default function PhotoDisplay() {
 	}, []);
 
 	return (
-		<div className="photo">
-			Title : {image.title};
-			<img clasName="pod" src={image.hdurl} alt="nasa img of the day" />; Whats
-			the story behind this photo? {image.explanation}
-			{/* {PhotoDisplay.map(photo => {
-                console.log(photo);
-				// return photo.hdurl;
-			})} */}
-		</div>
+		<>
+			<h2>
+				<Badge color="warning">Title : {image.title}</Badge>
+			</h2>
+			<img clasName="pod" src={image.url} alt="nasa img of the day" />;
+			<div>
+				<Alert color="success">
+					<h3 className="alert-heading">What's the story behind this photo?</h3>
+					<h5>{image.explanation}</h5>
+					<hr />
+					<p className="mb-0">Project By: Mosae Litsoane</p>
+				</Alert>
+			</div>
+		</>
 	);
 }
